2db08ec07dd6ed187a9dcc7c17e657b0dc9a5ba0
derek <djyoung@ucdavis.edu>
2019-10-29 17:22:01 -0700

    Functionality for both interactive and command-line running

--- python/metashape_control.py
+++ python/metashape_control.py
@@ -7,24 +7,32 @@
 
 import sys
 
-from python import metashape_pipeline_functions as meta
-from python import read_yaml
-
-### import the Metashape functionality
+### Add the Metashape functionality
 # If this is a first run from the standalone python module, need to copy the license file from the full metashape install: from python import metashape_license_setup
-#import Metashape
 
-## Read config data from command line argument
-config_file = sys.argv[0]
-
-## Alternatively, if running interacively read from here:
-config_file = "config/example.yml"
+## Define where to get the config file (only used if running interactively)
+manual_config_file = "config/example.yml"
 
 
+## Load custom modules: slightly different depending whether running interactively or via command line
+if hasattr(sys,"ps1"): # running interactively
+  from python import metashape_pipeline_functions as meta
+  from python import read_yaml  
+else: # running from command line
+    import metashape_pipeline_functions as meta
+    import read_yaml
+    
+## Load config file: slightly different depending whether running interactively or via command line
+if hasattr(sys,"ps1"): # running interactively
+    config_file = manual_config_file
+else: # running from command line
+    config_file = sys.argv[1]
+    
+
+## Parse the config file
 cfg = read_yaml.read_yaml(config_file)
 
-
-
+### Run the Metashape workflow
 doc, log, run_id = meta.project_setup(cfg)
 
 meta.enable_and_log_gpu(log)
