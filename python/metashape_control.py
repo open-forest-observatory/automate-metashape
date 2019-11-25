339c3767681136ff391475c3c1c57afaf9b39c70
Derek Young <derek.j.n.young@gmail.com>
2019-11-25 14:29:02 -0800

    YAML config file can select/skip processing steps
    Now it is only necessary to modify the config file. Metashape_control is just a framework of all the options for the YAML to select/deselect

--- python/metashape_control.py
+++ python/metashape_control.py
@@ -18,7 +18,6 @@ manual_config_file = "config/example.yml"
 try:  # running interactively
     from python import metashape_pipeline_functions as meta
     from python import read_yaml
-
     config_file = manual_config_file
 except:  # running from command line
     import metashape_pipeline_functions as meta
@@ -34,31 +33,44 @@ doc, log, run_id = meta.project_setup(cfg)
 
 meta.enable_and_log_gpu(log)
 
-meta.add_photos(doc, cfg)
+if cfg["load_project"] == "":  # only add photos if this is a brand new project, not based off an existing project
+    meta.add_photos(doc, cfg)
 
-meta.add_gcps(doc, cfg)
+if cfg["addGCPs"]["enabled"]:
+    meta.add_gcps(doc, cfg)
 
-meta.align_photos(doc, log, cfg)
+if cfg["alignPhotos"]["enabled"]:
+    meta.align_photos(doc, log, cfg)
 
-# meta.calibrate_reflectance(doc, cfg)
+if cfg["calibrateReflectance"]["enabled"]:
+    meta.calibrate_reflectance(doc, cfg)
 
-meta.optimize_cameras(doc, cfg)
+if cfg["optimizeCameras"]["enabled"]:
+    meta.optimize_cameras(doc, cfg)
 
-meta.build_depth_maps(doc, log, cfg)
+if cfg["buildDepthMaps"]["enabled"]:
+    meta.build_depth_maps(doc, log, cfg)
 
-meta.build_dense_cloud(doc, log, cfg)
+if cfg["buildDenseCloud"]["enabled"]:
+    meta.build_dense_cloud(doc, log, cfg)
 
-meta.classify_ground_points(doc, log, cfg)
+if cfg["classifyGroundPoints"]["enabled"]:
+    meta.classify_ground_points(doc, log, cfg)
 
-meta.build_dem(doc, log, cfg)
+if cfg["buildDem"]["enabled"]:
+    meta.build_dem(doc, log, cfg)
 
-meta.build_orthomosaic(doc, log, cfg)
+if cfg["buildOrthomosaic"]["enabled"]:
+    meta.build_orthomosaic(doc, log, cfg)
 
-meta.export_dem(doc, log, run_id, cfg)
+if cfg["exportDem"]["enabled"]:
+    meta.export_dem(doc, log, run_id, cfg)
 
-meta.export_orthomosaic(doc, log, run_id, cfg)
+if cfg["exportOrthomosaic"]["enabled"]:
+    meta.export_orthomosaic(doc, log, run_id, cfg)
 
-meta.export_points(doc, log, run_id, cfg)
+if cfg["exportPoints"]["enabled"]:
+    meta.export_points(doc, log, run_id, cfg)
 
 meta.export_report(doc, run_id, cfg)
 
