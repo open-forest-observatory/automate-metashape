d48a29cfe7d9235bbb59912d5245ac3dcea337d6
Derek <derek.j.n.young@gmail.com>
2021-01-22 12:15:35 -0800

    Don't modify export resolution (not possible to change anyway)

--- python/metashape_workflow_functions.py
+++ python/metashape_workflow_functions.py
@@ -516,7 +516,6 @@ def build_dem(doc, log_file, run_id, cfg):
         if cfg["buildDem"]["export"]:
             doc.chunk.exportRaster(path=output_file,
                                    projection=projection,
-                                   resolution = cfg["buildDem"]["export_resolution"],
                                    nodata_value=cfg["buildDem"]["nodata"],
                                    source_data=Metashape.ElevationData,
                                    image_compression=compression)
@@ -530,7 +529,6 @@ def build_dem(doc, log_file, run_id, cfg):
         if cfg["buildDem"]["export"]:
             doc.chunk.exportRaster(path=output_file,
                                    projection=projection,
-                                   resolution = cfg["buildDem"]["export_resolution"],
                                    nodata_value=cfg["buildDem"]["nodata"],
                                    source_data=Metashape.ElevationData,
                                    image_compression=compression)
