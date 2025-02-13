e5c46e177ca85d022a4a04d82c0d68d48d476df2
arnavakula <arnavakula@users.noreply.github.com>
2025-02-13 22:50:12 +0000

    Apply black formatting changes

--- python/metashape_workflow.py
+++ python/metashape_workflow.py
@@ -13,7 +13,9 @@ from pathlib import Path
 
 ## Define where to get the config file (only used if running interactively)
 # manual_config_file = "config/config-base.yml"
-manual_config_file = Path(Path(__file__).parent, "..", "config", "config-base.yml").resolve()
+manual_config_file = Path(
+    Path(__file__).parent, "..", "config", "config-base.yml"
+).resolve()
 # ---- If not running interactively, the config file should be supplied as the command-line argument after the python script, e.g.: python metashape_workflow.py config.yml
 
 
@@ -30,7 +32,9 @@ def parse_args():
         + "All other arguments are optional overrides to the corresponding entry in that config"
     )
     parser.add_argument(
-        "--config_file", default=manual_config_file, help="A path to a yaml config file."
+        "--config_file",
+        default=manual_config_file,
+        help="A path to a yaml config file.",
     )
     parser.add_argument(
         "--photo-path",
