0fe89516d518fd77f8ae459ce6c4f1b9d277e675
David Russell <djrussell@ucdavis.edu>
2025-05-30 16:33:02 +0000

    Move config updates into the main workflow functions script

--- python/metashape_workflow_functions.py
+++ python/metashape_workflow_functions.py
@@ -4,6 +4,8 @@ import glob
 import os
 import platform
 import re
+from pathlib import Path
+import collections
 
 # Import the fuctionality we need to make time stamps to measure performance
 import time
@@ -14,6 +16,46 @@ import yaml
 
 
 #### Helper functions
+def recursive_update(d, u):
+    """ "
+    Recursively update dictionary `d` with any keys from `u`. New keys contained in `u` but not in `d`
+    will be created.
+
+    Taken from: https://stackoverflow.com/questions/3232943/update-value-of-a-nested-dictionary-of-varying-depth
+    """
+    for k, v in u.items():
+        if isinstance(v, collections.abc.Mapping):
+            d[k] = recursive_update(d.get(k, {}), v)
+        else:
+            d[k] = v
+    return d
+
+def make_derived_yaml(input_path: str, output_path: str, override_options: dict):
+    """Create a new config file by reading one file and updating specific values
+
+    Args:
+        input_path (str):
+            The path to the yaml config file to load from
+        output_path (str):
+            The path to the yaml config file to write out. Containing folder will be created if needed.
+        override_options (dict):
+            A potentially-nested dictionary
+    """
+    # Read the input config
+    with open(input_path, "r") as ymlfile:
+        base_cfg = yaml.load(ymlfile, Loader=yaml.SafeLoader)
+
+    # Update the values in the base config
+    updated_config = recursive_update(base_cfg, override_options)
+
+    # Create the output folder if needed
+    Path(output_path).parent.mkdir(exist_ok=True, parents=True)
+
+    # Write out the updated config
+    with open(output_path, "w") as ymlfile:
+        # Preserve the initial order of keys for readability
+        yaml.dump(updated_config, ymlfile, sort_keys=False)
+
 def convert_objects(a_dict):
     """
     Convert strings that refer to metashape objects (e.g. "Metashape.MoasicBlending") into metashape objects
@@ -105,17 +147,12 @@ class MetashapeWorkflow:
             self.cfg = yaml.load(ymlfile, Loader=yaml.SafeLoader)
 
     def override_config(self, override_dict):
-        # Remove any override options that are None
-        override_dict = {k: v for k, v in override_dict.items() if v is not None}
-
-        # Since the CLI parser has nargs="+" for the photo_path, it will always be a list of values
-        # even if only one is provided. To match the format of the yaml parser, if only one value
-        # is provided, transform from a list of length one to just the value in that list
-        if "photo_path" in override_dict and len(override_dict["photo_path"]) == 1:
-            override_dict["photo_path"] = override_dict["photo_path"][0]
-
+        """
+        Update self.cfg using a potentially-nested dictionary of override values, in the same
+        stucture as the yaml config file.
+        """
         # Update any of the fields in the override dict to that value
-        self.cfg.update(override_dict)
+        self.cfg = recursive_update(self.cfg, override_dict)
 
     #### Functions for each major step in Metashape
 
