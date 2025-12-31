b1acd4d0d6da718f880fb1c4f8ba02d8e801fbf0
youngdjn <youngdjn@users.noreply.github.com>
2025-12-31 00:56:43 +0000

    Apply black formatting changes

--- python/metashape_workflow_functions.py
+++ python/metashape_workflow_functions.py
@@ -6,6 +6,7 @@ import json
 import os
 import platform
 import re
+
 # Import the fuctionality we need to make time stamps to measure performance
 import time
 from pathlib import Path
@@ -685,7 +686,7 @@ class MetashapeWorkflow:
         # Extract GPU names from device list
         gpu_names = []
         for device in gpu_devices:
-            gpu_names.append(device['name'])
+            gpu_names.append(device["name"])
         gpustring = ", ".join(gpu_names)
 
         return {
